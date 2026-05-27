aa65e64f96a20d44ae60234273b3a316922b73d2
Park Jaeyoung <qkrwodud0714@naver.com>
2026-05-27 14:13:08 +0900

    주택 가격 예측 LinearRegression을 이용하여

---
+++ main.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression 
+from sklearn.metrics import mean_squared_error, r2_score
+
+# 데이터셋 로드 (일단 모든 feature에 데이터를 사용)
+housing = fetch_california_housing()
+X = housing.data
+y = housing.target
+
+# train/test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# 머신러닝 모델 학습(회귀 : LinearResgression)
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# 예측 및 정확도 출력
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print(mse)
+print(r2)
