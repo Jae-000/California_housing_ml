a06d5f74be440c45b6380f97358fd979c8ffde24
Park Jaeyoung <qkrwodud0714@naver.com>
2026-05-28 14:32:38 +0900

    third commit 랜덤포레스트를 이용해서 학습

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression 
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 # 데이터셋 로드 (일단 모든 feature에 데이터를 사용)
 housing = fetch_california_housing()
-X = housing.data[:, [0, 2, 6, 1]] 
+X = housing.data
 y = housing.target
 
 # train/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 머신러닝 모델 학습(회귀 : LinearResgression)
-model = LinearRegression()
+# 머신러닝 모델 학습(회귀 : RandomForestRegressor)
+model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 # 예측 및 정확도 출력
